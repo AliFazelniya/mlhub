0823bd248ee6ef25eaada5063cfff7a54a9720df
Ali Fazelniya <alifazelniya.1384@gmail.com>
2026-05-20 00:23:54 +0330

    add first files of part3

---
+++ neural_network_with_matlab/part3/BackpropMmt.m
@@ -0,0 +1,65 @@
+clear all
+
+function[W1, W2] = BackpropMmtf(W1, W2, X, D)
+    alpha = 0.9;
+    beta = 0.9;
+
+    mmt1 = zeros(size(W1));
+    mmt2 = zeros(size(W2));
+
+    [N, ~] = size(X);
+    for k = 1:N
+        x = X(k, :)';
+        d = D(k);
+
+        v1 = W1 * x;
+        y1 = Sigmoid(v1);
+
+        v = W2 * y1;
+        y = Sigmoid(v);
+
+        e = d - y;
+        delta = e .* y .* (1 - y);
+
+        e1 = W2' * delta;
+        delta1 = e1 .* y1 .* (1 - y1);
+
+        dW1 = alpha * delta1 * x';
+        mmt1 = dW1 + beta*mmt1;
+        W1 = W1 + mmt1;
+
+        dW2 = alpha * delta * y1';
+        mmt2 = dW2 + beta*mmt2;
+        W2 = W2 + mmt2;
+    end
+end
+
+function y = Sigmoid(x)
+    y = 1 ./ (1 + exp(-x));
+end
+
+X = [0 0 1;
+     0 1 1;
+     1 0 1;
+     1 1 1];
+
+D = [0; 1; 1; 0];
+
+W1 = 2*rand(4, 3) - 1;
+W2 = 2*rand(1, 4) - 1;
+
+for epoch = 1:10000
+    [W1, W2] = BackpropMmtf(W1, W2, X, D);
+end
+
+N = 4;
+
+for k = 1:N
+    x = X(k, :)';
+    v1 = W1 * x;
+    y1 = Sigmoid(v1);
+    v = W2 * y1;
+    y = Sigmoid(v);
+
+    disp(y)
+end
--- quera/data-analysis/2. Numpy/Practices/4. Iris_flower/calc_no_loop.py
+++
@@ -1,4 +0,0 @@
-# coding: utf-8
-import numpy as np
-def calc_no_loop(new_points, points):
-    return np.sum((new_points[:, np.newaxis, :] - points[np.newaxis, :, :]) ** 2, axis=2)
