df79cb0c109bb9bfc60f8ed821d3ddef97cba768
Ali Fazelniya <alifazelniya.1384@gmail.com>
2026-05-20 17:28:28 +0330

    last code file of part 3

---
+++ neural_network_with_matlab/part3/BackpropCE.m
@@ -0,0 +1,56 @@
+clear all
+
+function [W1, W2] = BackpropCEf(W1, W2, X, D)
+    alpha = 0.1;
+    
+    [N, ~] = size(X);
+    for k = 1:N
+        x = X(k, :)';
+        d = D(k);
+
+        v1 = W1 * x;
+        y1 = Sigmoid(v1);
+
+        v = W2 * y1;
+        y = Sigmoid(v);
+
+        e = d - y;
+        delta = e;
+
+        e1 = W2' * delta;
+        delta1 = y1 .* (1 - y1) .* e1;
+
+        W1 = W1 + alpha * delta1 * x';
+        W2 = W2 + alpha * delta * y1';
+    end
+end
+
+function y = Sigmoid(x)
+    y = 1 ./ (1 + exp(-x));
+end
+
+X = [0 0 1;
+     0 1 1;
+     1 0 1;
+     1 1 1];
+
+D = [0; 1; 1; 0];
+
+W1 = 2 * rand(4, 3) - 1;
+W2 = 2 * rand(1, 4) - 1;
+
+for epoch = 1:10000
+    [W1, W2] = BackpropCEf(W1, W2, X, D);
+end
+
+N = 4;
+
+for k = 1:N
+    x = X(k, :)';
+    v1 = W1 * x;
+    y1 = Sigmoid(v1);
+    v = W2 * y1;
+    y = Sigmoid(v);
+
+    disp(y)
+end
